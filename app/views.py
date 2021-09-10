59c3b81d5f6f0c81d094309b09ecd2df4850467f
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-10 15:10:57 +0300

    initializing the news app

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('index.html')
