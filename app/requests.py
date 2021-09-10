e8c6d8c9e1e1c33ca7c6ae7a890394b7cafdcf51
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-10 20:38:06 +0300

    adding function to process results

--- app/requests.py
+++ app/requests.py
@@ -29,3 +29,30 @@ def get_news(category):
 
 
     return news_results
+
+# function to process results
+
+def process_results(news_list):
+    '''
+    Function  that processes the news result and transform them to a list of Objects
+
+    Args:
+        news_list: A list of dictionaries that contain news details
+
+    Returns :
+        news_results: A list of news objects
+    '''
+    news_results = []
+    for news_item in news_list:
+        id = news_item.get('id')
+        title = news_item.get('original_title')
+        overview = news_item.get('overview')
+        poster = news_item.get('poster_path')
+        vote_average = news_item.get('vote_average')
+        vote_count = news_item.get('vote_count')
+
+        if poster:
+            news_object = News(id,title,overview,poster,vote_average,vote_count)
+            news_results.append(news_object)
+
+    return news_results
