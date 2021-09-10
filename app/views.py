22d0a4a034d4736a8afc32f1118ca7e3838b640d
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-10 16:07:42 +0300

    adding news api key

--- app/views.py
+++ app/views.py
@@ -8,8 +8,8 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'Hello World'
-    return render_template('index.html', message =message)
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title)
 
 # dynamic routes
 @app.route('/news/<int:news_id>')
