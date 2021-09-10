635b86ae0808373b0278dacb395cc7c010d0568d
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-10 15:26:27 +0300

    adding dynamic routes to news page

--- app/views.py
+++ app/views.py
@@ -8,4 +8,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = 'Hello World'
+    return render_template('index.html', message =message)
+
+# dynamic routes
+@app.route('/news/<news_id>')
+def news(news_id):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    return render_template('news.html',id = news_id)
