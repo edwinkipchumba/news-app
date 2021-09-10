ce0a771ed2cb6f8860d696c92b4c9977d0db56c2
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-10 15:38:05 +0300

    adding parent and children configuration

--- app/config.py
+++ app/config.py
@@ -3,3 +3,12 @@ class Config:
     General configuration parent class
     '''
     pass
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
