69f7634c9557661bac9d63f7af008028c3b2b6d3
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-10 15:43:09 +0300

    adding updates for configuration in run.py

--- app/config.py
+++ app/config.py
@@ -12,3 +12,13 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     pass
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
