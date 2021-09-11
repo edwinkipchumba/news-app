599a614eb6ca1b1df15df1aa56fa0444d300f73a
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-11 19:55:01 +0300

    adding style to the navbar

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ class Config:
     General configuration parent class
     '''
     NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
-    # CAT_API_URL='https://newsapi.org/v2/everything?q={}&sortBy=relevancy&apiKey={}'
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
     CAT_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
 
@@ -15,7 +14,6 @@ class Config:
 class ProdConfig(Config):
     '''
     Production  configuration child class
-
     Args:
         Config: The parent configuration class with General configuration settings
     '''
@@ -25,7 +23,6 @@ class ProdConfig(Config):
 class DevConfig(Config):
     '''
     Development  configuration child class
-
     Args:
         Config: The parent configuration class with General configuration settings
     '''
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export MOVIE_API_KEY=<Your api key>
+export NEWS_API_KEY='c8d240ed91ee42f48337113c5100471e'
 # export SECRET_KEY=<Your secret key>
 
 python3.8 manage.py server
