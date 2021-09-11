a7c1d18b2c7ae9e1a6d8fa43e247b3052173d681
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-11 14:16:49 +0300

    update index.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,26 +1,35 @@
-from flask import render_template
-from app import app
-from .requests import get_news
+from flask import render_template,request,redirect,url_for
+from . import main
+from flask import render_template,request,redirect,url_for
+from ..request import get_source,article_source,get_category,get_headlines
 
-# Views
-@app.route('/')
+#our views
+@main.route('/')
 def index():
+    '''
+    Root function returning index/home page with data
+    '''
+    source= get_source()
+    headlines = get_headlines()
+    return render_template('index.html',sources=source, headlines = headlines)
+
+@main.route('/article/<id>')
+def article(id):
 
     '''
-    View root page function that returns the index page and its data
+    View article page function that returns the various article details page and its data
     '''
+    # title= 'Articles'
+    articles = article_source(id)
+    return render_template('article.html',articles= articles,id=id )
 
-    # Getting popular news
-    popular_news = get_news('popular')
-    print(popular_news)
-    title = 'Home - Welcome to The best News Review Website Online'
-    return render_template('index.html', title = title,popular = popular_news)
-
-# dynamic routes
-@app.route('/news/<int:news_id>')
-def news(news_id):
-
+@main.route('/categories/<cat_name>')
+def category(cat_name):
     '''
-    View news page function that returns the news details page and its data
+    function to return the categories.html page and its content
     '''
-    return render_template('news.html',id = news_id)
+    category = get_category(cat_name)
+    title = f'{cat_name}'
+    cat = cat_name
+
+    return render_template('categories.html',title = title,category = category, cat= cat_name)
