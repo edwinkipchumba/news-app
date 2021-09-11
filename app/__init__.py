aca51fd6d9c6d4f39694a6475fa622b6c9efcff1
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-11 14:01:58 +0300

    adding function to render error 404

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,27 @@
 from flask import Flask
-from .config import DevConfig
+from flask_bootstrap import Bootstrap
+from config import config_options
 
-from app import views
+bootstrap = Bootstrap()
 
-# Initializing application
-app = Flask(__name__,instance_relative_config =True)
+def create_app(config_name):
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+    app = Flask(__name__)
 
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
+    # Initializing flask extensions
+    bootstrap.init_app(app)
 
+# Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+# setting config
+    from .request import configure_request
+    configure_request(app)
+
+    # Will add the views and forms
+
+    return app
--- app/request.py
+++ app/request.py
@@ -1,78 +1,125 @@
-import newsapi
-from app import app
 import urllib.request,json
-# from .models import news
-from newsapi import NewsApiClient
-
-# News = news.News
-sources = newsapi.get_sources()
+from .models import Article, Category, Source , Headlines
 
 # Getting api key
-api_key = app.config['NEWS_API_KEY']
+api_key = None
+# Getting source url
+source_url= None
+# Getting source url
+cat_url= None
 
-# Getting the movie base url
-# base_url = app.config["NEWS_API_BASE_URL"]
-# /v2/top-headlines
-top_headlines = newsapi.get_top_headlines(q='bitcoin',
-                                          sources='bbc-news,the-verge',
-                                          category='business',
-                                          language='en',
-                                          country='us')
-
-# /v2/everything
-all_articles = newsapi.get_everything(q='bitcoin',
-                                      sources='bbc-news,the-verge',
-                                      domains='bbc.co.uk,techcrunch.com',
-                                      from_param='2017-12-01',
-                                      to='2017-12-12',
-                                      language='en',
-                                      sort_by='relevancy',
-                                      page=2)
+def configure_request(app):
+    global api_key, source_url, cat_url
+    api_key = app.config['NEWS_API_KEY']
+    source_url= app.config['NEWS_API_SOURCE_URL']
+    cat_url=app.config['CAT_API_URL']
 
 
-
-def get_news(category):
+def get_source():
     '''
-    Function that gets the json response to our url request
+    Function that gets the json response to url request
     '''
-    get_news_url = base_url.format(category,api_key)
+    get_source_url= source_url.format(api_key)
+    # print(get_source_url)
+    with urllib.request.urlopen(get_source_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
 
-    with urllib.request.urlopen(get_news_url) as url:
-        get_news_data = url.read()
-        get_news_response = json.loads(get_news_data)
+        source_results = None
 
-        news_results = None
+        if get_sources_response['sources']:
+            source_results_list = get_sources_response['sources']
+            source_results = process_results(source_results_list)
 
-        if get_news_response['results']:
-            news_results_list = get_news_response['results']
-            news_results = process_results(news_results_list)
+    return source_results
 
-
-    return news_results
-
-# function to process results
-
-def process_results(news_list):
+def process_results(source_list):
     '''
-    Function  that processes the news result and transform them to a list of Objects
-
+    function to process results and transform them to a list of objects
     Args:
-        news_list: A list of dictionaries that contain news details
-
-    Returns :
-        news_results: A list of news objects
+        source_list:dictionary cotaining source details
+    Returns:
+        source_results: A list of source objects
     '''
-    news_results = []
-    for news_item in news_list:
-        id = news_item.get('id')
-        title = news_item.get('original_title')
-        overview = news_item.get('overview')
-        poster = news_item.get('poster_path')
-        vote_average = news_item.get('vote_average')
-        vote_count = news_item.get('vote_count')
+    source_results = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        if id:
+            source_object = Source(id,name,description,url)
+            source_results.append(source_object)
 
-        if poster:
-            news_object = News(id,title,overview,poster,vote_average,vote_count)
-            news_results.append(news_object)
+    return source_results
 
-    return news_results
+def article_source(id):
+    article_source_url = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'.format(id,api_key)
+    print(article_source_url)
+    with urllib.request.urlopen(article_source_url) as url:
+        article_source_data = url.read()
+        article_source_response = json.loads(article_source_data)
+
+        article_source_results = None
+
+        if article_source_response['articles']:
+            article_source_list = article_source_response['articles']
+            article_source_results = process_articles_results(article_source_list)
+
+
+    return article_source_results
+
+def process_articles_results(news):
+    '''
+    function that processes the json files of articles from the api key
+    '''
+    article_source_results = []
+    for article in news:
+        author = article.get('author')
+        description = article.get('description')
+        time = article.get('publishedAt')
+        url = article.get('urlToImage')
+        image = article.get('url')
+        title = article.get ('title')
+
+        if url:
+            article_objects = Article(author,description,time,image,url,title)
+            article_source_results.append(article_objects)
+
+    return article_source_results
+
+def get_category(cat_name):
+    '''
+    function that gets the response to the category json
+    '''
+    get_category_url = cat_url.format(cat_name,api_key)
+    print(get_category_url)
+    with urllib.request.urlopen(get_category_url) as url:
+        get_category_data = url.read()
+        get_cartegory_response = json.loads(get_category_data)
+
+        get_cartegory_results = None
+
+        if get_cartegory_response['articles']:
+            get_cartegory_list = get_cartegory_response['articles']
+            get_cartegory_results = process_articles_results(get_cartegory_list)
+
+    return get_cartegory_results
+
+def get_headlines():
+    '''
+    function that gets the response to the category json
+    '''
+    get_headlines_url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'.format(api_key)
+    print(get_headlines_url)
+    with urllib.request.urlopen(get_headlines_url) as url:
+        get_headlines_data = url.read()
+        get_headlines_response = json.loads(get_headlines_data)
+
+        get_headlines_results = None
+
+        if get_headlines_response['articles']:
+            get_headlines_list = get_headlines_response['articles']
+            get_headlines_results = process_articles_results(get_headlines_list)
+
+    return get_headlines_results
